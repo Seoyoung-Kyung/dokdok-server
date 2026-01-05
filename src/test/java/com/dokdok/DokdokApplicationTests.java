f56aa83df364be61bb0a2991b69c847362153961
CHO GUNHEE <106148197+dkqpeo@users.noreply.github.com>
2026-01-05 13:22:23 +0900

    CD TEST (#1)
    * CD TEST

--- src/test/java/com/dokdok/DokdokApplicationTests.java
+++ src/test/java/com/dokdok/DokdokApplicationTests.java
@@ -2,9 +2,11 @@ package com.dokdok;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class DkdkApplicationTests {
+@ActiveProfiles("test")
+class DokdokApplicationTests {
 
 	@Test
 	void contextLoads() {
