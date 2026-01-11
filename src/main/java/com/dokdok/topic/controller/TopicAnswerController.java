ce87bea08f919953f0b1a96f813018c50c4e2354
JWoong-01 <yju407@naver.com>
2026-01-11 19:37:28 +0900

    refactor: api 구조 통일

--- src/main/java/com/dokdok/topic/controller/TopicAnswerController.java
+++ src/main/java/com/dokdok/topic/controller/TopicAnswerController.java
@@ -1,31 +1,26 @@
 package com.dokdok.topic.controller;
 
 import com.dokdok.global.response.ApiResponse;
+import com.dokdok.topic.api.TopicAnswerApi;
 import com.dokdok.topic.dto.request.TopicAnswerRequest;
 import com.dokdok.topic.dto.response.TopicAnswerResponse;
 import com.dokdok.topic.service.TopicAnswerService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/gatherings/{gathering_id}/meetings/{meeting_id}/topics/{topic_id}/answers")
-public class TopicAnswerController {
+public class TopicAnswerController implements TopicAnswerApi {
 
     private final TopicAnswerService topicAnswerService;
 
-    @PostMapping
+    @Override
     public ResponseEntity<ApiResponse<TopicAnswerResponse>> createAnswer(
-            @PathVariable("gathering_id") Long gatheringId,
-            @PathVariable("meeting_id") Long meetingId,
-            @PathVariable("topic_id") Long topicId,
-            @RequestBody @Valid TopicAnswerRequest request
+            Long gatheringId,
+            Long meetingId,
+            Long topicId,
+            TopicAnswerRequest request
     ) {
         // TODO: @AuthenticationPrincipal에서 userId 추출
         Long userId = 1L;
