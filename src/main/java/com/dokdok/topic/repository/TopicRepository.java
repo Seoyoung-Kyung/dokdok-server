6616ce271dcc483ce4c7e485b97b9c22b5199229
onuyyy <qazxsw9026@naver.com>
2026-01-11 17:03:55 +0900

    feat : 약속 상세 로직에서 필요하여 추가

--- src/main/java/com/dokdok/topic/repository/TopicRepository.java
+++ src/main/java/com/dokdok/topic/repository/TopicRepository.java
@@ -1,4 +1,10 @@
 package com.dokdok.topic.repository;
 
-public interface TopicRepository {
+import com.dokdok.topic.entity.Topic;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TopicRepository extends JpaRepository<Topic, Long> {
+    List<Topic> findAllByMeetingId(Long meetingId);
 }
