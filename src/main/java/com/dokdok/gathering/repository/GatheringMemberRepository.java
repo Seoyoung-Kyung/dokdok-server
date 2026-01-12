84f0f045b5d452b6382020efea848fd1539af8a3
juhyun <guswnfl5@naver.com>
2026-01-12 14:41:59 +0900

    fix: 충돌 해결

--- src/main/java/com/dokdok/gathering/repository/GatheringMemberRepository.java
+++ src/main/java/com/dokdok/gathering/repository/GatheringMemberRepository.java
@@ -1,7 +1,6 @@
 package com.dokdok.gathering.repository;
 
 import com.dokdok.gathering.entity.GatheringMember;
-<<<<<<< HEAD
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
