35cbb6fc6a143aa20c716cd67cd0b27ff8f93553
U-hee <uhee0112@gmail.com>
2026-01-13 21:48:03 +0900

    refactor: api 수정

--- src/main/java/com/dokdok/book/dto/request/BookCreateRequest.java
+++ src/main/java/com/dokdok/book/dto/request/BookCreateRequest.java
@@ -1,14 +1,15 @@
 package com.dokdok.book.dto.request;
 
 import com.dokdok.book.entity.Book;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
 public record BookCreateRequest(
-    String title,
-    String authors,
-    String publisher,
-    String isbn,
+    @NotBlank(message = "제목은 필수입니다.") String title,
+    @NotBlank(message = "저자는 필수입니다.") String authors,
+    @NotBlank(message = "출판사는 필수입니다.") String publisher,
+    @NotBlank(message = "ISBN은 필수입니다.") String isbn,
     String thumbnail
 ) {
     public Book of() {
