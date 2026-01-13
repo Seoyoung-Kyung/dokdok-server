603c4cbd740c0432e5a038eff23b07da96dad047
onuyyy <qazxsw9026@naver.com>
2026-01-13 14:26:31 +0900

    feat : 약속 참가 취소자가 등록한 주제 삭제

--- src/main/java/com/dokdok/topic/repository/TopicRepository.java
+++ src/main/java/com/dokdok/topic/repository/TopicRepository.java
@@ -2,7 +2,10 @@ package com.dokdok.topic.repository;
 
 import com.dokdok.topic.entity.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +13,17 @@ import java.util.List;
 public interface TopicRepository extends JpaRepository<Topic, Long> {
 
     List<Topic> findAllByMeetingId(Long meetingId);
+
+    @Modifying
+    @Query("""
+      UPDATE Topic t
+      SET t.deletedAt = CURRENT_TIMESTAMP
+      WHERE t.meeting.id = :meetingId
+      AND t.proposedBy.id = :userId
+      AND t.deletedAt IS NULL
+      """)
+    void softDeleteByMeetingIdAndProposedById(
+            @Param("meetingId") Long meetingId,
+            @Param("userId") Long userId
+    );
 }
