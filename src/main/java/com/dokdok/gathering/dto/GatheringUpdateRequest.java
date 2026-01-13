c194f6f3222651b46ba52709812efb2c3e2f3650
juhyun <guswnfl5@naver.com>
2026-01-13 10:38:42 +0900

    feat : 모임명에 @Pattern 정규식 사용

--- src/main/java/com/dokdok/gathering/dto/GatheringUpdateRequest.java
+++ src/main/java/com/dokdok/gathering/dto/GatheringUpdateRequest.java
@@ -1,14 +1,15 @@
 package com.dokdok.gathering.dto;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import org.hibernate.validator.constraints.Length;
 
 @Builder
 public record GatheringUpdateRequest(
-        @NotBlank(message = "모임명은 공백일 수 없습니다.")
-        @Length(max=255, message = "모임며은 255자를 초과할 수 없습니다.")
+        @Pattern(regexp = "^(?!\\s*$).{0,12}$",
+                message = "모임명은 공백만 포함할 수 없으며 12자를 초과할 수 없습니다")
         String gatheringName,
+        @Length(max=150, message = "모임 설명은 150자를 초과할 수 없습니다.")
         String description
 ) {
 }
