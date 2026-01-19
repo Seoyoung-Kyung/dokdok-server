471e1bdb156402128cbed696997206c4ae558579
juhyun <guswnfl5@naver.com>
2026-01-19 17:14:23 +0900

    feat : 공동 회고 comment 삭제

--- src/main/java/com/dokdok/retrospective/service/RetrospectiveValidator.java
+++ src/main/java/com/dokdok/retrospective/service/RetrospectiveValidator.java
@@ -2,6 +2,7 @@ package com.dokdok.retrospective.service;
 
 import com.dokdok.gathering.service.GatheringValidator;
 import com.dokdok.meeting.service.MeetingValidator;
+import com.dokdok.retrospective.entity.MeetingRetrospective;
 import com.dokdok.retrospective.exception.RetrospectiveErrorCode;
 import com.dokdok.retrospective.exception.RetrospectiveException;
 import com.dokdok.retrospective.repository.PersonalRetrospectiveRepository;
@@ -38,4 +39,14 @@ public class RetrospectiveValidator {
         }
     }
 
+    public void validateMeetingRetrospectiveDeletePermission(MeetingRetrospective retrospective, Long userId) {
+        // 삭제하는 사람이 작성자 본인인지 확인
+        if (retrospective.getCreatedBy().getId().equals(userId)) {
+            return;
+        }
+
+        Long gatheringId = retrospective.getMeeting().getGathering().getId();
+        // 삭제하는 사람이 모임장인지 확인
+        gatheringValidator.validateLeader(gatheringId, userId);
+    }
 }
