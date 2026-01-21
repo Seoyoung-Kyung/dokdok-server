1023d57db15db5678b4cf53e6748bc762f8b2667
CHO GUNHEE <lan0184@naver.com>
2026-01-21 12:59:54 +0900

    refactor : 모임 생성 시 텍스트 길이 제한 설정.

--- src/main/java/com/dokdok/gathering/dto/request/GatheringCreateRequest.java
+++ src/main/java/com/dokdok/gathering/dto/request/GatheringCreateRequest.java
@@ -1,10 +1,15 @@
 package com.dokdok.gathering.dto.request;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record GatheringCreateRequest(
         @NotBlank(message = "모임의 이름은 필수입력입니다.")
+        @Size(max = 12, message = "모임 이름은 12자 이내로 입력해주세요.")
         String gatheringName,
+
+        @Size(max = 150, message = "모임 설명은 150자 이내로 입력해주세요.")
         String gatheringDescription
 ) {
 }
