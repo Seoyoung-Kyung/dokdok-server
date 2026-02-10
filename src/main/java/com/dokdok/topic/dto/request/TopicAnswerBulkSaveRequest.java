6f3d7c8bfe41a8bb28506b9c61ce878459e8ae92
JWoong-01 <yju407@naver.com>
2026-02-10 23:07:42 +0900

    refactor: 빈 사전답변 제출 가능으로 변경

--- src/main/java/com/dokdok/topic/dto/request/TopicAnswerBulkSaveRequest.java
+++ src/main/java/com/dokdok/topic/dto/request/TopicAnswerBulkSaveRequest.java
@@ -3,7 +3,6 @@ package com.dokdok.topic.dto.request;
 import com.dokdok.book.dto.request.BookReviewRequest;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -25,9 +24,8 @@ public record TopicAnswerBulkSaveRequest(
             @NotNull
             @Schema(description = "토픽 ID", example = "1")
             Long topicId,
-            @NotBlank(message = "설명은 필수입니다")
             @Size(max = 1000, message = "설명은 1000자 이내여야 합니다")
-            @Schema(description = "답변 내용", example = "이 책은 작은 행동의 반복이 인생을 바꾼다고 생각합니다.")
+            @Schema(description = "답변 내용", example = "이 책은 작은 행동의 반복이 인생을 바꾼다고 생각합니다.", nullable = true)
             String content
     ) {
     }
